805696849fe17ce89623e011ff6561bec29a609f
Muhammad Shahzaib <mshahazaib88@gmail.com>
2026-02-18 20:38:07 +0500

    intial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Muhammad Shahzaib")
+col1,col2 = st.columns(2)
+
+with col1:
+    st.image('my imh.jpg')
+with col2:
+    st.write('''As a passionate and results-driven AI Engineer, I specialize in designing, developing, and deploying machine learning and deep learning models to solve complex real-world problems. With a strong foundation in Python and hands-on experience with frameworks like TensorFlow and PyTorch, I am adept at every stage of the AI project lifecycle, from data preprocessing and feature engineering to model training, evaluation, and deployment.''')
